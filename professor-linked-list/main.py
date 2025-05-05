c0bf879e38e7636a00b4a03449f07bbc6f58cf74
Kodie <kodii.tulip@gmail.com>
2025-05-05 19:50:44 -0300

    linked list professors version -> exercise version

--- professor-linked-list/main.py
+++ professor-linked-list/main.py
@@ -39,6 +39,22 @@ class ListADT(ABC):
         """Retorna o tamanho da lista"""
         ...
 
+    @abstractmethod
+    def remove_all(self, item):
+        ...
+
+    @abstractmethod
+    def remove_at(self, index):
+        ...
+
+    @abstractmethod
+    def append(self, item):
+        ...
+
+    @abstractmethod
+    def replace(self, index, item):
+        ...
+
 
 class Node[T]:
 
@@ -68,6 +84,44 @@ class LinkedList[E](ListADT):
         self._head: Optional[Node[E]] = Node[E](elem) if elem else None  # Atenção ao manipular essa referência
         self._tail: Optional[Node[E]] = self._head if elem else None  # Facilita a inserção no fim da lista
         self._length: int = 1 if elem else 0
+
+    def remove_all(self, item: E) -> None:
+        if self.empty():
+            return
+
+        while self._head.get_element() == item:
+            self._head = self._head.get_next()
+        itr: Optional[Node[E]] = self._head
+        while itr:
+            next_ = itr.get_next()
+            if next_ and next_.get_element() == item:
+                itr.set_next(next_.get_next())
+            else:
+                itr = itr.get_next()
+
+    def remove_at(self, index: int) -> None:
+        pos: int = 0
+        itr: Optional[Node[E]] = self._head
+        while itr.get_next() and pos < index - 1:
+            itr = itr.get_next()
+            pos += 1
+        to_remove = itr.get_next()
+        itr.set_next(to_remove.get_next())
+
+    def append(self, item: E) -> None:
+        if self.empty():
+            return self.__insert_at_beginning(item)
+        return self.__insert_at_end(item)
+
+    def replace(self, index, item: E) -> None:
+        if index > self._length - 1:
+            return
+        pos: int = 0
+        itr: Optional[Node[E]] = self._head
+        while itr.get_next() and pos < index:
+            itr = itr.get_next()
+            pos += 1
+        itr.set_element(item)
 
     def insert(self, index: int, elem: E) -> None:
         # a inserção pode acontecer em três locais: início, meio e fim da lista
@@ -156,18 +210,23 @@ class LinkedList[E](ListADT):
         while not result and pos < self._length:  # lembrando que not None é o mesmo que True
             if aux.get_element() is elem:
                 result = pos
+                break
             aux = aux.get_next()
             pos += 1
         return result  # se o elemento não estiver na lista, retorna None
 
     def length(self) -> int:
-        return self._length
+        if self.empty():
+            return 0
+        count: int = 1
+        itr: Optional[Node[E]] = self._head
+        while itr.get_next():
+            itr = itr.get_next()
+            count += 1
+        return count
 
     def empty(self) -> bool:
-        result = False
-        if not self._head:
-            result = True
-        return result
+        return not self._head
 
     def __str__(self) -> str:
         if self.empty():
@@ -198,6 +257,10 @@ class DoublyLinkedList(ListADT):
         def elem(self):
             return self._elem
 
+        @elem.setter
+        def elem(self, value):
+            self._elem = value
+
         @property
         def next(self):
             return self._next
@@ -221,13 +284,52 @@ class DoublyLinkedList(ListADT):
         self._trailer._prev = self._header
         self._length = 0
 
+    def remove_all(self, item) -> None:
+        if self.empty():
+            return
+
+        while self._header.elem == item:
+            self._header = self._header.next
+        itr = self._header
+        while itr:
+            next_ = itr.next
+            if next_ and next_.elem == item:
+                itr.next = next_.next
+            else:
+                itr = itr.next
+
+    def remove_at(self, index: int) -> None:
+        pos: int = 0
+        itr = self._header
+        while itr.next and pos < index:
+            itr = itr.next
+            pos += 1
+        to_remove = itr.next
+        itr.next = to_remove.next
+
+    def append(self, item) -> None:
+        if self.empty():
+            return self.insert(0, item)
+        return self.insert(self.length() - 1, item)
+
+    def replace(self, index, item) -> None:
+        length: int = self.length()
+        if index >= length:
+            index = length
+        pos: int = 0
+        itr = self._header
+        while itr.next and pos <= index:
+            itr = itr.next
+            pos += 1
+        itr.elem = item
+
     def insert(self, index, elem):
         if index >= self._length:  # se o índice se inserção passado for maior que a lista
             index = self._length  # atualiza para o último índice
         if self.empty():  # Caso da lista vazia
             new_node = self._DoublyNode(elem, self._header, self._trailer)
-            self._header._next = new_node
-            self._trailer._prev = new_node
+            self._header.next = new_node
+            self._trailer.prev = new_node
         elif index == 0:  # caso da inserção na primeira posição da lista
             new_node = self._DoublyNode(elem, self._header, self._header.next)
             self._header.next.prev = new_node
@@ -287,15 +389,23 @@ class DoublyLinkedList(ListADT):
         while not result and pos < self._length:  # lembrando que not None Ã© o mesmo que True
             if this.elem is elem:
                 result = pos
+                break
             this = this.next
             pos += 1
         return result  # se o elemento não estiver na lista, retorna None
 
     def length(self):
-        return self._length
+        if self.empty():
+            return 0
+        count: int = 1
+        itr = self._header
+        while itr.next:
+            itr = itr.next
+            count += 1
+        return count
 
     def empty(self):
-        return self._length == 0
+        return not self._header
 
     def __str__(self):
         if not self.empty():
@@ -323,16 +433,10 @@ if __name__ == '__main__':
     lista.insert(20, 20)
     print(lista)
 
-    lista.insert(0, 3.14)
-    print(lista)
-
-    lista.insert(1, 'no meio')
-    print(lista)
-
-    lista.insert(3, 'no meio')
-    print(lista)
-
     lista.insert(10000, 'loooonge')
+    print(lista)
+
+    lista.insert(0, 20)
     print(lista)
 
     print('contagem')
@@ -341,7 +445,7 @@ if __name__ == '__main__':
     print(lista.count(20))
 
     print('indices')
-    print(lista.index('no meio'))
+    print(lista.index(20))
     print(lista.index('loooonge'))
     print(lista.index('bla'))
     print('remoção')
@@ -350,34 +454,102 @@ if __name__ == '__main__':
     print(lista)
 
     print('vamos remover...')
-    lista.remove('no meio')
+    lista.remove(20)
     print(lista)
 
     lista.remove('teste')
     print(lista)
 
-    lista.remove(3.14)
+    print('length')
+    print(lista.length())
+
+    print('append')
+    lista.append(20)
+    lista.append('oi')
+    lista.append(20)
+    lista.append(20)
+    lista.append('test')
+    lista.append(20)
+    lista.append(20)
     print(lista)
-    print('-----------------------------')
-    print()
-    print('-----------------------------')
-    print('Doubly Linked List')
+
+    print('remove_at')
+    lista.remove_at(3)
+    print(lista)
+
+    print('remove_all')
+    lista.remove_all(20)
+    print(lista)
+
+    print('replace')
+    lista.replace(1, 'oi')
+    print(lista)
+
+    print('''
+-------------
+
+-------------
+Double Link List
+    ''')
+
     lista = DoublyLinkedList()
-    lista.insert(0, 0)
     print(lista)
-    lista.insert(1, 1)
+
+    print('inserção')
+    lista.insert(0, 'teste')
     print(lista)
-    lista.insert(2, 2)
+
+    lista.insert(20, 20)
     print(lista)
-    lista.insert(0, 3)
+
+    lista.insert(10000, 'loooonge')
     print(lista)
-    lista.insert(1000, 4)
+
+    lista.insert(0, 20)
     print(lista)
-    lista.insert(3, 5)
+
+    print('contagem')
+    print(lista.count('no meio'))
+    print(lista.count('oi'))
+    print(lista.count(20))
+
+    print('indices')
+    print(lista.index(20))
+    print(lista.index('loooonge'))
+    print(lista.index('bla'))
+    print('remoção')
+
+    print('lista inicial')
     print(lista)
-    print(lista.index(1))
-    print(lista.count(1))
-    lista.insert(0, 1)
-    print(lista.count(1))
-    lista.remove(5)
+
+    print('vamos remover...')
+    lista.remove(20)
+    print(lista)
+
+    lista.remove('teste')
+    print(lista)
+
+    print('length')
+    print(lista.length())
+
+    print('append')
+    lista.append(20)
+    lista.append('oi')
+    lista.append(20)
+    lista.append(20)
+    lista.append('test')
+    lista.append(20)
+    lista.append(20)
+    print(lista)
+
+    print('remove_at')
+    lista.remove_at(3)
+    print(lista)
+
+    print('remove_all')
+    lista.remove_all(20)
+    print(lista)
+
+    print('replace')
+    lista.replace(1, 'oi')
     print(lista)
